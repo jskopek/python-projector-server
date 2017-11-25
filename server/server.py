e2b230372a59c5240fcbbad750111d2016fbc3af
Jean-Marc Skopek <jskopek@gmail.com>
2017-11-24 19:57:18 -0800

    Moved painting code into class

--- server/server.py
+++ server/server.py
@@ -8,22 +8,33 @@ import json
 
 logging.basicConfig(level=logging.DEBUG, format='[%(levelname)s] (%(threadName)-15s) %(message)s')
 
+class Painter(object):
+    def __init__(self, **kwargs):
+        self.master = Tk()
+        self.width = self.master.winfo_screenwidth()
+        self.height = self.master.winfo_screenheight()
+
+        self.fullscreen_manager = FullScreenApp(self.master)
+        self.canvas = Canvas(self.master, width=self.width, height=self.height)
+        self.canvas.pack()
+
+    def draw_oval(self, pctX, pctY, pctSize):
+        oX1 = self.width * pctX
+        oY1 = self.height * pctY
+        oX2 = oX1 + (self.width * pctSize)
+        oY2 = oY1 + (self.width * pctSize)
+
+        self.canvas.create_oval(oX1, oY1, oX2, oY2, fill='#000')
+        logging.debug('drawOval: Create Oval: oX1=%s oY1=%s oX2=%s oY2=%s' % (oX1, oY1, oX2, oY2))
+
+    def start(self):
+        mainloop()
+
+
 def socketServerMessageReceived(client, server, message):
     data = json.loads(message)
     logging.debug('socketServerMessageReceived with contents: %s' % message)
-    drawOval(master, canvas, pctX=data[0], pctY=data[1], pctSize=data[2])
-
-def drawOval(master, canvas, pctX, pctY, pctSize):
-    width = master.winfo_screenwidth()
-    height = master.winfo_screenheight()
-
-    oX1 = width * pctX
-    oY1 = height * pctY
-    oX2 = oX1 + (width * pctSize)
-    oY2 = oY1 + (width * pctSize)
-
-    canvas.create_oval(oX1, oY1, oX2, oY2, fill='#000')
-    logging.debug('drawOval: Create Oval: oX1=%s oY1=%s oX2=%s oY2=%s' % (oX1, oY1, oX2, oY2))
+    painter.draw_oval(pctX=data[0], pctY=data[1], pctSize=data[2])
 
 def socketServer():
     server = WebsocketServer(5002)
@@ -34,8 +45,5 @@ t = threading.Thread(name='socketServer', target=socketServer)
 t.setDaemon(True)
 t.start()
 
-master = Tk()
-app = FullScreenApp(master)
-canvas = Canvas(master, width=master.winfo_screenwidth(), height=master.winfo_screenheight())
-canvas.pack()
-mainloop()
+painter = Painter()
+painter.start()
